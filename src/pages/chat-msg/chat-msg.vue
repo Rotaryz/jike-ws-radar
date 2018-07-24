cd685dd8a23a2f4b70b54a61039247aedcfda364
panranran <ran@jkweixin.com>
2018-07-24 11:46:45 +0800

    聊天页面优化

--- src/pages/chat-msg/chat-msg.vue
+++ src/pages/chat-msg/chat-msg.vue
@@ -421,6 +421,10 @@
       padding: 6px 15px
       display: flex
       align-items: flex-end
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
       .submit-btn
         width: 50px
         height: 36px
